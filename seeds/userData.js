ef7955e3908bf6d6e74649def39f44f6825a4dc9
Ian Daluz <janiandaluz@yahoo.com>
2024-07-22 08:06:48 -0600

    changed minor codes

--- seeds/userData.js
+++ seeds/userData.js
@@ -3,28 +3,28 @@ const { User } = require("../models");
 // Array of dummy blog post data
 const userData = [
   {
-    username: "user1",
-    email: "user1@example.com",
+    username: "DaenarysStormborn",
+    email: "dany@example.com",
     password: "password1",
   },
   {
-    username: "user2",
-    email: "user2@example.com",
+    username: "RhaenyraTargaryen",
+    email: "rhae2@example.com",
     password: "password2",
   },
   {
-    username: "user3",
-    email: "user3@example.com",
+    username: "JohnSnow",
+    email: "jonjon@example.com",
     password: "password3",
   },
   {
-    username: "user4",
-    email: "user4@example.com",
+    username: "CerseiLannister",
+    email: "cerlan@example.com",
     password: "password4",
   },
   {
-    username: "user5",
-    email: "user5@example.com",
+    username: "TomenBaratheon",
+    email: "tomy@example.com",
     password: "password5",
   },
 
