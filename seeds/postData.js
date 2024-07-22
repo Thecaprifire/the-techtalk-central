547a1e7d430361bf70fb516f788440135ae95c79
Ian Daluz <janiandaluz@yahoo.com>
2024-07-22 08:13:41 -0600

    changed the title and content

--- seeds/postData.js
+++ seeds/postData.js
@@ -2,29 +2,28 @@ const { Post } = require("../models");
 
 const postData = [
   {
-    title: "First Blog Post",
-    content: "This is the content of the first blog post.",
+    title: "Why My Code Works Only on Fridays",
+    content: "Ever noticed how some code only decides to work on Fridays? Maybe it’s tired from a long week of bugs and just wants to take the weekend off!",
     user_id: 1,
   },
-  // add more dummy blog posts here
   {
-    title: "Second Blog Post",
-    content: "This is the content of the second blog post.",
+    title: "The Great Tab vs. Space Debate: An Unending Saga",
+    content: "Tabs or spaces? The coding world’s version of the chicken or the egg. Let's dive deep into why neither side will ever give up.",
     user_id: 2,
   },
   {
-    title: "Third Blog Post",
-    content: "This is the content of the third blog post.",
+    title: "Debugging: The Art of Destroying Bugs and Sanity Simultaneously",
+    content: "Debugging is like being the detective in a crime movie where you are also the murderer. Let’s explore the ups and downs of tracking those pesky bugs!",
     user_id: 3,
   },
   {
-    title: "Fourth Blog Post",
-    content: "This is the content of the fourth blog post.",
+    title: "How to Name Your Variables: Avoiding ‘Thingy’ and ‘Stuff’",
+    content: "Naming variables is an art. Learn how to avoid the dreaded ‘thingy’ and ‘stuff’ in your codebase and impress your team with your naming prowess.",
     user_id: 4,
   },
   {
-    title: "Fifth Blog Post",
-    content: "This is the content of the fifth blog post.",
+    title: "Coffee: The Fuel That Powers Coders and Their Code",
+    content: "Without coffee, would code even exist? Let's take a look at how this magical bean juice keeps the world of programming spinning.",
     user_id: 5,
   }
 ];
