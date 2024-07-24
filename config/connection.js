20973bfb153d5f6132b117eae4274002a69471f7
Ian Daluz <janiandaluz@yahoo.com>
2024-07-24 00:18:08 -0600

    connect to postgresql

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,15 @@
-const Sequelize = require("sequelize");
-require("dotenv").config();
+let sequelize;
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: "localhost",
-    dialect: "postgres",
-    port: 5432,
-  }
-);
-
-sequelize.authenticate().then(() => {
-  console.log('Connection has been established successfully.');
-}).catch((err) => {
-  console.error('Unable to connect to the database:', err);
-});
-
-module.exports = sequelize;
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'postgres',
+    },
+  );
+}
